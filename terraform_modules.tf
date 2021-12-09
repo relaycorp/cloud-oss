93b66c89ffa9f1f39a8635c82e83c92f93fffb76
Gus Narea <gnarea@users.noreply.github.com>
2021-12-09 18:55:28 +0000

    Import terraform-github-oss-project

---
+++ terraform_modules.tf
@@ -0,0 +1,8 @@
+module "tfmod_oss_project" {
+  source  = "relaycorp/oss-project/github"
+  version = "1.0.1"
+
+  name = "terraform-github-oss-project"
+  description = "Terraform module for open source projects by Relaycorp"
+  licence = "MIT"
+}
