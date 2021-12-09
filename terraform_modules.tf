df2f6b1f0642c89bd538e747b24bbbe611f780a9
Gus Narea <gnarea@users.noreply.github.com>
2021-12-09 19:34:19 +0000

    Set URL for terraform-github-oss-project

--- terraform_modules.tf
+++ terraform_modules.tf
@@ -2,7 +2,8 @@ module "tfmod_oss_project" {
   source  = "relaycorp/oss-project/github"
   version = "1.0.1"
 
-  name        = "terraform-github-oss-project"
-  description = "Terraform module for open source projects by Relaycorp"
-  licence     = "MIT"
+  name         = "terraform-github-oss-project"
+  description  = "Terraform module for open source projects by Relaycorp"
+  homepage_url = "https://registry.terraform.io/modules/relaycorp/oss-project/github/latest"
+  licence      = "MIT"
 }
