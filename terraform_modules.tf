d8b5707f1eb512f6441f176d1d995eef15791eb2
Gus Narea <gnarea@users.noreply.github.com>
2021-12-20 12:49:38 +0000

    Upgrade relaycorp/oss-project/github to v2.2 in terraform-github-oss-project

--- terraform_modules.tf
+++ terraform_modules.tf
@@ -1,6 +1,6 @@
 module "tfmod_oss_project" {
   source  = "relaycorp/oss-project/github"
-  version = "1.0.5"
+  version = "2.2.0"
 
   name         = "terraform-github-oss-project"
   description  = "Terraform module for open source projects by Relaycorp"
