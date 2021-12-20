dbaf102203b0139529e7981dbac9318ad698ab79
Gus Narea <gnarea@users.noreply.github.com>
2021-12-20 15:26:37 +0000

    Import relaynet-internet-gateway

---
+++ awala_servers.tf
@@ -0,0 +1,15 @@
+module "awala_internet_gateway" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.2.0"
+
+  name         = "relaynet-internet-gateway"
+  description  = "The Awala-Internet Gateway"
+  homepage_url = "https://docs.relaycorp.tech/relaynet-internet-gateway/"
+  licence      = "agpl"
+  topics       = ["awala", "awala-gateway"]
+  ci_contexts  = ["ci"]
+  main_branch  = "master"
+
+  pages_source_path = "/docs"
+  pages_source_branch = "master"
+}
