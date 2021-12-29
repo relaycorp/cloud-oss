61f6da98aba7da7e34ee4b516eac14d3dcbbe2c1
Gus Narea <gnarea@users.noreply.github.com>
2021-12-29 12:34:16 +0000

    Import cloud-gateway (#9)

---
+++ cloud.tf
@@ -0,0 +1,21 @@
+locals {
+  cloud_gateway_envs = ["frankfurt"]
+}
+
+module "cloud_gateway" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.4.0"
+
+  name         = "cloud-gateway"
+  description  = "Infrastructure as Code and configuration for all Awala-Internet Gateways run by Relaycorp"
+  homepage_url = ""
+  licence      = "agpl"
+  topics       = ["awala", "awala-gateway", "cloud"]
+
+  ci_contexts = flatten([
+    ["ci", "Terraform Cloud/Relaycorp/cloud-gateway"],
+    [for env in local.cloud_gateway_envs : "Terraform Cloud/Relaycorp/gateway-${env}"],
+    [for env in local.cloud_gateway_envs : "gateway-${env}-gke-deployment-preview (relaycorp-cloud-gateway)"],
+  ])
+  support_releases = false
+}
