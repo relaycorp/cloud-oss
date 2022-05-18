f95d5a16fe83542539098b6fd6a1985463676eca
Gus Narea <gnarea@users.noreply.github.com>
2022-05-18 15:45:29 +0100

    Node.js libs CI: Drop Node.js v12 and require Node.js v18

--- nodejs.tf
+++ nodejs.tf
@@ -1,13 +1,13 @@
 locals {
   nodejs_lib_ci_contexts = [
-    "ci / test-nodejs12-ubuntu-latest",
-    "ci / test-nodejs12-macos-latest",
-    "ci / test-nodejs12-windows-latest",
     "ci / test-nodejs14-ubuntu-latest",
     "ci / test-nodejs14-macos-latest",
     "ci / test-nodejs14-windows-latest",
     "ci / test-nodejs16-ubuntu-latest",
     "ci / test-nodejs16-macos-latest",
     "ci / test-nodejs16-windows-latest",
+    "ci / test-nodejs18-ubuntu-latest",
+    "ci / test-nodejs18-macos-latest",
+    "ci / test-nodejs18-windows-latest",
   ]
 }
