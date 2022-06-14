073533e88cd556333b396091f27428be3b83e8cc
Gus Narea <gnarea@users.noreply.github.com>
2022-06-14 12:59:04 +0100

    Import awala-gateway-desktop and awala-ping-desktop

---
+++ awala_desktop.tf
@@ -0,0 +1,24 @@
+module "awala_gateway_desktop" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name         = "awala-gateway-desktop"
+  description  = "Awala Gateway for Desktop"
+  homepage_url = "https://awala.network/users/download"
+  licence      = "gplv3"
+  topics       = ["awala", "awala-gateway", "windows", "linux", "macos"]
+  ci_contexts  = ["ci-ubuntu-latest", "ci-macos-latest", "ci-windows-latest"]
+  main_branch  = "master"
+}
+
+module "awala_ping_desktop" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name         = "awala-ping-desktop"
+  description  = "Awala Ping for Desktop"
+  homepage_url = "https://www.npmjs.com/package/@relaycorp/awala-ping"
+  licence      = "gplv3"
+  topics       = ["awala", "awala-service-ping", "windows", "linux", "macos"]
+  ci_contexts  = ["ci-ubuntu-latest", "ci-macos-latest", "ci-windows-latest"]
+}
