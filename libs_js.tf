9f0fc26bb6ae83b0e4831eaaf0dbdf564e44d502
Gus Narea <gnarea@users.noreply.github.com>
2022-08-25 12:17:22 +0100

    Create dnssec-js repo
    For Vera

--- libs_js.tf
+++ libs_js.tf
@@ -49,3 +49,17 @@ module "it_duplex_ack_js" {
   topics       = ["javascript", "iterables", "streams"]
   ci_contexts  = local.nodejs_lib_ci_contexts
 }
+
+module "dnssec_js" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name         = "dnssec-js"
+  description  = "DNSSEC resolver implementation for Node.js"
+  homepage_url = "https://docs.relaycorp.tech/dnssec-js/"
+  licence      = "mit"
+  topics       = ["dnssec"]
+  ci_contexts  = local.nodejs_lib_ci_contexts
+
+  pages_source_path = "/"
+}
