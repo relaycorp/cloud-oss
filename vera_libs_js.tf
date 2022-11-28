2b2266e3a1ff961d0ad8c98292de5624db216195
Gus Narea <gnarea@users.noreply.github.com>
2022-11-28 14:08:51 +0000

    vera-js: Lower case GH topics

--- vera_libs_js.tf
+++ vera_libs_js.tf
@@ -6,7 +6,7 @@ module "vera_js" {
   description  = "VeraId library for Node.js"
   homepage_url = "https://www.npmjs.com/package/@relaycorp/vera"
   licence      = "mit"
-  topics       = ["VeraId"]
+  topics       = ["vera", "veraid"]
   ci_contexts  = local.nodejs_lib_min_v16_ci_contexts
 
   pages_source_path = "/"
