cbdcc523df144e75d3592b38a4893655492e6102
Gus Narea <gnarea@users.noreply.github.com>
2022-11-28 14:41:56 +0000

    veraid-js: Rename back to vera-js

--- veraid_libs_js.tf
+++ veraid_libs_js.tf
@@ -2,10 +2,10 @@ module "veraid_js" {
   source  = "relaycorp/oss-project/github"
   version = "2.6.0"
 
-  name         = "veraid-js"
+  name         = "vera-js"
   description  = "VeraId library for Node.js"
-  homepage_url = "https://www.npmjs.com/package/@relaycorp/veraid"
+  homepage_url = "https://www.npmjs.com/package/@relaycorp/vera"
   licence      = "mit"
-  topics       = ["veraid"]
+  topics       = ["veraid", "vera"]
   ci_contexts  = local.nodejs_lib_min_v16_ci_contexts
 }
