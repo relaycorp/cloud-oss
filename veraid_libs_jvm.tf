f8e75f865c4c64c59ab555b09f57d6dc09cebae9
Gus Narea <gnarea@users.noreply.github.com>
2023-02-06 11:06:30 +0000

    Create vera-jvm project

---
+++ veraid_libs_jvm.tf
@@ -0,0 +1,11 @@
+module "veraid_jvm" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name         = "vera-jvm"
+  description  = "VeraId library for the JVM"
+  homepage_url = "https://search.maven.org/artifact/tech.relaycorp/vera"
+  licence      = "apache2"
+  topics       = ["veraid", "vera"]
+  ci_contexts  = local.lib_jvm_ci_contexts
+}
