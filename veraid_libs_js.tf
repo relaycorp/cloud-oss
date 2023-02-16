7f493a0d40564280be5b74aefae9eb646ef4db07
Gus Narea <gnarea@users.noreply.github.com>
2023-02-16 12:18:00 +0000

    Rename "vera-" prefix to "veraid-"

--- veraid_libs_js.tf
+++ veraid_libs_js.tf
@@ -2,7 +2,7 @@ module "veraid_js" {
   source  = "relaycorp/oss-project/github"
   version = "2.6.0"
 
-  name         = "vera-js"
+  name         = "veraid-js"
   description  = "VeraId library for Node.js"
   homepage_url = "https://www.npmjs.com/package/@relaycorp/veraid"
   licence      = "mit"
--- veraid_libs_jvm.tf
+++ veraid_libs_jvm.tf
@@ -2,9 +2,9 @@ module "veraid_jvm" {
   source  = "relaycorp/oss-project/github"
   version = "2.6.0"
 
-  name         = "vera-jvm"
+  name         = "veraid-jvm"
   description  = "VeraId library for the JVM"
-  homepage_url = "https://search.maven.org/artifact/tech.relaycorp/vera"
+  homepage_url = "https://search.maven.org/artifact/tech.relaycorp/veraid"
   licence      = "apache2"
   topics       = local.vera_topics
   ci_contexts  = local.lib_jvm_ci_contexts
