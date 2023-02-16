96f8b30ce43d39ce10a25f086a296944aa11e0a4
Gus Narea <gnarea@users.noreply.github.com>
2023-02-16 11:02:32 +0000

    Create VeraId CA Server repo (#18)

---
+++ vera.tf
@@ -0,0 +1,3 @@
+locals {
+  vera_topics = ["veraid", "vera"]
+}
---
+++ veraid_servers.tf
@@ -0,0 +1,12 @@
+module "veraid_ca_server" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name        = "veraid-authority"
+  description = "VeraId Certificate Authority (CA) server"
+  licence     = "agpl"
+  topics      = local.vera_topics
+  ci_contexts = ["ci / ci"]
+
+  pages_source_path = "/"
+}
