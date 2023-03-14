604ff2e5cf61e6a533a1b79a968b38cdc4089420
Gus Narea <gnarea@users.noreply.github.com>
2023-03-14 18:41:27 +0100

    Complete porting of Awala JVM libs (#22)

--- awala_libs_jvm.tf
+++ awala_libs_jvm.tf
@@ -38,3 +38,84 @@ module "awala_lib_testing_jvm" {
 
   pages_source_path = "/"
 }
+
+module "awala_lib_cogrpc_jvm" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name         = "awala-cogrpc-jvm"
+  description  = "JVM implementation of the Awala CogRPC binding"
+  homepage_url = "https://docs.relaycorp.tech/awala-cogrpc-jvm/"
+  licence      = "apache2"
+  topics = [
+    "awala",
+    "awala-binding-cogrpc",
+    "java",
+    "jvm",
+    "kotlin"
+  ]
+  ci_contexts = local.lib_jvm_ci_contexts
+  main_branch = "master"
+
+  pages_source_path = "/"
+}
+
+module "awala_lib_cogrpc_okhttp_jvm" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name         = "awala-cogrpc-okhttp-jvm"
+  description  = "gRPC OkHTTP channel provider for Awala's CogRPC JVM implementation"
+  homepage_url = "https://docs.relaycorp.tech/awala-cogrpc-okhttp-jvm/"
+  licence      = "apache2"
+  topics = [
+    "awala",
+    "awala-binding-cogrpc",
+    "java",
+    "jvm",
+    "kotlin",
+    "android",
+    "okhttp"
+  ]
+  ci_contexts = local.lib_jvm_ci_contexts
+  main_branch = "master"
+}
+
+module "awala_lib_poweb_jvm" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name         = "awala-poweb-jvm"
+  description  = "JVM implementation of the Awala PoWeb binding"
+  homepage_url = ""
+  licence      = "apache2"
+  topics = [
+    "awala",
+    "awala-binding-poweb",
+    "java",
+    "jvm",
+    "kotlin",
+  "android"]
+  ci_contexts = local.lib_jvm_ci_contexts
+  main_branch = "master"
+
+  pages_source_path = "/"
+}
+
+module "awala_lib_keystore_jvm" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name         = "awala-keystore-file-jvm"
+  description  = "JVM implementation of file-based Private and Public Key Stores for Awala"
+  homepage_url = ""
+  licence      = "apache2"
+  topics = [
+    "awala",
+    "awala-keystore",
+    "java",
+    "jvm",
+    "kotlin"
+  ]
+  ci_contexts = local.lib_jvm_ci_contexts
+}
