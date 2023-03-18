1dcbd4a652d7799f6002fc4ab75403afd06a32bb
Gus Narea <gnarea@users.noreply.github.com>
2023-03-18 18:13:34 +0100

    Fix Maven Central URLs

--- veraid_libs_jvm.tf
+++ veraid_libs_jvm.tf
@@ -4,7 +4,7 @@ module "veraid_jvm" {
 
   name         = "veraid-jvm"
   description  = "VeraId library for the JVM"
-  homepage_url = "https://central.sonatype.com/artifact/tech.relaycorp/veraid"
+  homepage_url = "https://central.sonatype.com/search?q=veraid&namespace=tech.relaycorp"
   licence      = "apache2"
   topics       = local.veraid_topics
   ci_contexts  = local.lib_jvm_ci_contexts
