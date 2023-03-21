acbf864916f79fa3c75a3c549eea032656967035
Gus Narea <gnarea@users.noreply.github.com>
2023-03-21 14:41:06 +0100

    Create kms-js

--- libs_js.tf
+++ libs_js.tf
@@ -35,6 +35,17 @@ module "object_storage_js" {
   pages_source_path = "/"
 }
 
+module "kms_js" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name         = "kms-js"
+  description  = "Node.js client that abstracts Key Management Services like GCP KMS"
+  licence      = "mit"
+  topics       = ["kms", "key-management-system", "gcp-kms", "vault"]
+  ci_contexts  = local.nodejs_lib_ci_contexts
+}
+
 module "pino_cloud_js" {
   source  = "relaycorp/oss-project/github"
   version = "2.6.0"
