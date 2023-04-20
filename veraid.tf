110fe3e61e5e9ae89a9df346bb442e2c1d5604bb
Gus Narea <gnarea@users.noreply.github.com>
2023-04-20 11:50:05 +0100

    veraid: Merge all resources into a single file (#26)

--- veraid.tf
+++ veraid.tf
@@ -1,3 +1,42 @@
 locals {
   veraid_topics = ["veraid", "vera"]
 }
+
+module "veraid_js" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name         = "veraid-js"
+  description  = "VeraId library for Node.js"
+  homepage_url = "https://www.npmjs.com/package/@relaycorp/veraid"
+  licence      = "mit"
+  topics       = local.veraid_topics
+  ci_contexts  = local.nodejs_lib_min_v16_ci_contexts
+
+  pages_source_path = "/"
+}
+
+module "veraid_jvm" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name         = "veraid-jvm"
+  description  = "VeraId library for the JVM"
+  homepage_url = "https://docs.relaycorp.tech/veraid-jvm/"
+  licence      = "apache2"
+  topics       = local.veraid_topics
+  ci_contexts  = local.lib_jvm_ci_contexts
+
+  pages_source_path = "/"
+}
+
+module "veraid_ca_server" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name        = "veraid-authority"
+  description = "VeraId Certificate Authority (CA) server"
+  licence     = "agpl"
+  topics      = local.veraid_topics
+  ci_contexts = ["ci / ci"]
+}
