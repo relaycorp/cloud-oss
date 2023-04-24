b27f63cb77366f9e370b1d0679cb90524ec99732
Gus Narea <gnarea@users.noreply.github.com>
2023-04-24 12:37:16 +0100

    veraid-authority-js: Require Node.js 18

--- ci.tf
+++ ci.tf
@@ -1,12 +1,14 @@
 locals {
-  nodejs_lib_min_v16_ci_contexts = [
-    "ci / test-nodejs16-ubuntu-latest",
-    "ci / test-nodejs16-macos-latest",
-    "ci / test-nodejs16-windows-latest",
+  nodejs_lib_min_v18_ci_contexts = [
     "ci / test-nodejs18-ubuntu-latest",
     "ci / test-nodejs18-macos-latest",
     "ci / test-nodejs18-windows-latest",
   ]
+  nodejs_lib_min_v16_ci_contexts = concat([
+    "ci / test-nodejs16-ubuntu-latest",
+    "ci / test-nodejs16-macos-latest",
+    "ci / test-nodejs16-windows-latest",
+  ], local.nodejs_lib_min_v18_ci_contexts)
   nodejs_lib_ci_contexts = concat([
     "ci / test-nodejs14-ubuntu-latest",
     "ci / test-nodejs14-macos-latest",
