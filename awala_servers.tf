6ef941e33448a8156970ca6ca0c549334a5dae10
Gus Narea <gnarea@users.noreply.github.com>
2023-04-30 13:35:52 +0100

    awala-endpoint-internet: Create

--- awala_servers.tf
+++ awala_servers.tf
@@ -29,3 +29,14 @@ module "awala_pong" {
   pages_source_path   = "/docs"
   pages_source_branch = "master"
 }
+
+module "awala_endpoint_internet" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.6.0"
+
+  name        = "awala-endpoint-internet"
+  description = "The Awala Internet Endpoint is a Message-Oriented Middleware (MOM) that allows server-side apps to communicate over Awala without implementing any of the networking or cryptography from the protocol suite."
+  licence     = "agpl"
+  topics      = ["awala", "awala-endpoint"]
+  ci_contexts = ["ci / ci"]
+}
