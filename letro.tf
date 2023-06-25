17d3b0fcb7eb6d25009ec0c069e0ef9e7b8352e6
Gus Narea <gnarea@users.noreply.github.com>
2023-06-25 18:14:07 +0100

    letro-android: Create

---
+++ letro.tf
@@ -0,0 +1,10 @@
+module "awala_courier_android" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.8.0"
+
+  name         = "letro-android"
+  description  = "Letro for Android"
+  licence      = "gplv3"
+  topics       = ["letro", "android"]
+  ci_contexts  = ["Run static checks and tests"]
+}
