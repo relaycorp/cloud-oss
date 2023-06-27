c71b83482d199962c32cc0c8bbf7a1fd6a87ed48
Gus Narea <gnarea@users.noreply.github.com>
2023-06-27 10:51:56 +0100

    letro-android: Configure Firebase Test Lab (#34)
    * letro-android: Configure Firebase Test Lab
    
    * empty
    
    * upgrade module
    
    * empty

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "bloco_team_members" {
   type        = list(string)
   description = "Email addresses of Bloco developers working on Awala"
 }
+
+variable "letro_external_devs" {
+  type        = list(string)
+  description = "Email addresses of external developers working on Awala"
+}
