565bb05a652ba05e3c31cd659edbb09565d4668f
Gus Narea <gnarea@users.noreply.github.com>
2023-07-31 15:44:13 +0100

    Require JS libs to be tests on Linux only

--- ci.tf
+++ ci.tf
@@ -1,18 +1,10 @@
 locals {
-  nodejs_lib_min_v20_ci_contexts = [
-    "ci / test-nodejs20-ubuntu-latest",
-    "ci / test-nodejs20-macos-latest",
-    "ci / test-nodejs20-windows-latest",
-  ]
+  nodejs_lib_min_v20_ci_contexts = ["ci / nodejs20"]
   nodejs_lib_min_v18_ci_contexts = concat([
-    "ci / test-nodejs18-ubuntu-latest",
-    "ci / test-nodejs18-macos-latest",
-    "ci / test-nodejs18-windows-latest",
+    "ci / nodejs18",
   ], local.nodejs_lib_min_v20_ci_contexts)
   nodejs_lib_ci_contexts = concat([
-    "ci / test-nodejs16-ubuntu-latest",
-    "ci / test-nodejs16-macos-latest",
-    "ci / test-nodejs16-windows-latest",
+    "ci / nodejs16",
   ], local.nodejs_lib_min_v18_ci_contexts)
 
   jvm_java11_ci_contexts = [
