176ac90872e322665a627d7497912545ec41ded2
Gus Narea <gnarea@users.noreply.github.com>
2024-09-10 15:58:20 +0100

    terraform-github-closed-source: Create (#100)

--- terraform_modules.tf
+++ terraform_modules.tf
@@ -10,3 +10,16 @@ module "tfmod_oss_project" {
 
   pages_enabled = false
 }
+
+module "tfmod_closed_source_project" {
+  source  = "relaycorp/oss-project/github"
+  version = "2.14.0"
+
+  name         = "terraform-github-closed-source"
+  description  = "Terraform module for closed source projects by Relaycorp"
+  homepage_url = "https://registry.terraform.io/modules/relaycorp/closed-source/github/latest"
+  licence      = "MIT"
+  ci_contexts  = ["ci-module / ci", "ci-example / ci"]
+
+  pages_enabled = false
+}
